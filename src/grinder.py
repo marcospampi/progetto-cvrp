589a6098e4cc6ec95d24ebf53289f2e47de21f10
marcospampi <cantabokca598@gmail.com>
2026-07-13 23:02:13 +0200

    improvement: save run

--- src/grinder.py
+++ src/grinder.py
@@ -22,7 +22,7 @@ def grinder(fe: int, seed: int, runs: int,  instances_path: str, profiles: dict,
 			for profile_name, profile in profiles.items():
 				tqdm.write(f"\tCurrent profile: {profile_name}")
 				np.random.seed(seed)
-				for i in range(runs):
+				for run in range(runs):
 					with ACO_MPSolver(instance, **profile) as solver:
 
 						start = time.time()
@@ -35,7 +35,7 @@ def grinder(fe: int, seed: int, runs: int,  instances_path: str, profiles: dict,
 							is_new_best = best_solution is None or best_solution.value > solution.value
 							if is_new_best:
 								best_solution = solution
-							yield instance, profile_name, profile, elapsed, i, iterations, solution, is_new_best
+							yield instance, profile_name, profile,run, elapsed, i, iterations, solution, is_new_best
 						
 						#solution_file = os.path.join(output_path, '_'.join([instance.name, profile_name]) + '.json' )
 						#if best_solution is None: continue
@@ -45,6 +45,7 @@ def grinder(fe: int, seed: int, runs: int,  instances_path: str, profiles: dict,
 		out.write(','.join([
 			'instance',
 			'profile',
+			'run',
 			'iteration',
 			'total_iterations',
 			'cost',
@@ -56,11 +57,12 @@ def grinder(fe: int, seed: int, runs: int,  instances_path: str, profiles: dict,
 		]))
 		out.write('\n')
 		for iteration in tqdm(runner()):
-			instance, profile_name, profile, elapsed, i,total_iterations, solution, is_new_best = iteration
+			instance, profile_name, profile,run, elapsed, i,total_iterations, solution, is_new_best = iteration
 			if not is_new_best: continue
 			out.write(",".join([
 				instance.name,
-				profile_name,			
+				profile_name,
+				str(run),
 				str(i),
 				str(total_iterations),
 				str(solution.value),
